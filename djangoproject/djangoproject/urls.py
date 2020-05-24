abb0a7c0dfd118479f163d148ece456408e3558f
João F <joao.francapf@gmail.com>
2020-05-23 23:33:29 -0300

    New reusable templates, views and basic forms

--- djangoproject/djangoproject/urls.py
+++ djangoproject/djangoproject/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     # this tells django when to serve the view
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('board/<int:pk>/', views.board_topics, name='board_topics')
+    path('board/<int:pk>/', views.board_topics, name='board_topics'),
+    path('board/<int:pk>/new', views.new_topic, name='new_topic'),
 ]
